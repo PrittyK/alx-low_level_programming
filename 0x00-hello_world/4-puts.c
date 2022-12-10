8e2e73801cde72db06b513534e087ea2c9c6ac6b
Prittyk <kakoozaibrahim21@gmail.com>
2022-12-10 08:27:48 -0800

    Hello, world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+/**
+ * main -serves as the entry point for the program
+ * description: returns the value of main
+ * Return: by default returns 0, if no errors otherwise returns non-zero value
+ */
+
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * description: returns the value of each input type
+ * prints the values
+ * Return: a value 0; if no error, value of non zero if ther is errors
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int ll;
+	float f;
+
+	printf("size of chae: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %d byte(s)\n", sizeof(i));
+	printf("size of long int: %ld  byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(ll));
+	printf("size of a float: %ld byte(s)\n" sizeof(f));
+	
+	return (0);
+}
