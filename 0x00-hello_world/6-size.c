6025b702d17b36a8fa8af9bee5ed7fef0d3edf03
Prittyk <kakoozaibrahim21@gmail.com>
2022-12-11 06:58:29 -0800

    test 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,19 +5,13 @@
  * prints the values
  * Return: a value 0; if no error, value of non zero if ther is errors
  */
-
 int main(void)
 {
-	char c;
-	int i;
-	long int li;
-	long long int ll;
-	float f;
-
-	printf("size of char: %d byte(s)\n", sizeof(c));
-	printf("size of an int: %d byte(s)\n", sizeof(i));
-	printf("size of long int: %d  byte(s)\n", sizeof(li));
-	printf("size of a long long int: %d byte(s)\n", sizeof(ll));
-	printf("size of a float: %d byte(s)\n" sizeof(f));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of long int: %d  byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
+
